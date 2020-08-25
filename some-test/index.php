cf0ccd051d8008f6670e7888b501679aa8811761
tvmichael <tvmichael@meta.ua>
2020-08-25 22:04:44 +0300

    test

---
+++ some-test/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+    <title>Is Ready</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+</head>
+
+<body>
+<div class="main">
+    <div class="cover black" data-color="black"></div>
+    <div class="container">
+        <h1 class="logo cursive">
+            Let's Make Something
+        </h1>
+        <div class="content">
+            <div class="form-group">
+                <label for=""></label>
+                <input class="form-control" type="text" value="" name="send-data-1">
+                <button id="send-data-1" class="btn btn-default">Send data</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/jq-test.js" type="text/javascript"></script>
+</body>
+</html>
+
