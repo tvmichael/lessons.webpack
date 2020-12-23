2e08df96a0678d5b5a0c1784419e8f08585250b4
tvmichael <michael.tmv@outlook.com>
2020-12-23 15:26:42 +0200

    asset

--- protected/assets/AppAsset.php
+++ protected/assets/AppAsset.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * https://www.yiiframework.com/doc/guide/2.0/en/structure-assets
  */
 
 namespace app\assets;
