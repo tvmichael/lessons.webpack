c573e611d116af61b80304d1a00c1cce32046593
Michael Torchuk <michael.tmv@outlook.com>
2021-12-10 21:32:16 +0200

    JsonEditor test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import {ConsoleTool} from "./utils/ConsoleTool";
-import {Tab} from 'bootstrap/js/dist/tab';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import 'jsoneditor/dist/jsoneditor.css'
+
+import {ConsoleTool} from "./utils/ConsoleTool";
+import {Tab} from 'bootstrap/js/dist/tab';
+import JSONEditor from 'jsoneditor/dist/jsoneditor.min.js';
+
 
 class WebpackLessons {
     constructor(element){
@@ -16,6 +19,26 @@ class WebpackLessons {
 
         let h1 = this.element.querySelector('h1');
         h1.textContent = 'Webpack lessons. Start!';
+
+        const container = document.getElementById("jsoneditor");
+        const options = {};
+        const editor = new JSONEditor(container, options);
+
+        // set json
+        const initialJson = {
+            "Array": [1, 2, 3],
+            "Boolean": true,
+            "Null": null,
+            "Number": 123,
+            "Object": {"a": "b", "c": "d"},
+            "String": "Hello World"
+        };
+
+        editor.set(initialJson);
+
+        // get json
+        const updatedJson = editor.get();
+
     }
 }
 
